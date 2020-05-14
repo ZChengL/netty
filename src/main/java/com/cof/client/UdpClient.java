da0b26773608b6627a4651b799598671fafa3cc2
liangzc@mapout.net <123456qaz>
2020-05-14 20:46:38 +0800

    修改pom,读取配置文件的端口

--- src/main/java/com/cof/client/UdpClient.java
+++ src/main/java/com/cof/client/UdpClient.java
@@ -11,24 +11,33 @@ import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author lilongke
  * @ClassName UdpClient
  **/
+@Slf4j
 public class UdpClient {
 
     private Channel channel;
 
-    public void init() {
+    public void init(String ip, int port) {
 
             EventLoopGroup group = new NioEventLoopGroup();
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.group(group)
                     .channel(NioDatagramChannel.class)
-                    .remoteAddress("127.0.0.1", 6666)
+                    .remoteAddress(ip, port)
                     .handler(new ChannelInitializer<NioDatagramChannel>() {
                         @Override
                         protected void initChannel(NioDatagramChannel ch) throws Exception {
@@ -40,21 +49,38 @@ public class UdpClient {
             try {
                 channel = bootstrap.bind(0).sync().channel();
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                log.error("连接失败+++++++++++++" + e);
             }
 
     }
 
-    public void send() {
-        channel.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer("abcdefg", CharsetUtil.UTF_8), new InetSocketAddress("127.0.0.1", 6666)));
+    public void send(String ip, int port) {
+        channel.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer("abcdefg", CharsetUtil.UTF_8), new InetSocketAddress(ip, port)));
     }
 
-    public static void main(String []args) throws InterruptedException {
+    public static void main(String []args) throws InterruptedException, IOException {
         UdpClient client = new UdpClient();
-        client.init();
+        Map<String, Object> ymlMap = getYmlMap();
+        int port = Integer.valueOf(((Map<String, Object>) ymlMap.get("UdpServer")).get("port").toString());
+        client.init("127.0.0.1", port);
+
         while (true) {
-            client.send();
+            client.send("127.0.0.1", port);
             Thread.sleep(2000);
         }
     }
+
+    public static Map<String,Object> getYmlMap(){
+        Map<String,Object> obj =null;
+        try {
+            Yaml yaml = new Yaml();
+            InputStream resourceAsStream = UdpClient.class.getClassLoader().getResourceAsStream("application.yml");
+            obj = (Map) yaml.load(resourceAsStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return obj;
+
+    }
+
 }
